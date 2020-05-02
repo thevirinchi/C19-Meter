5df1fb1505b98006d61f1dec69d65b300bf6c169
Z3T4Z00K <agarwal.archit17@gmail.com>
2020-05-03 00:54:11 +0530

    Added number fetching

--- app/src/main/java/com/z3t4z00k/c19meter/dashboard.java
+++ app/src/main/java/com/z3t4z00k/c19meter/dashboard.java
@@ -1,7 +1,20 @@
 package com.z3t4z00k.c19meter;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
@@ -14,21 +27,78 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
 public class dashboard extends AppCompatActivity {
 
+    String URL = "https://c19meterphp.herokuapp.com/index.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        LineChart casesChart = findViewById(R.id.casesChart);
+        final TextView death = findViewById(R.id.deaths);
+        final TextView recov = findViewById(R.id.recoveries);
+        final TextView cases = findViewById(R.id.cases);
+
+        class Login extends AsyncTask<Void, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                RequestHandler requestHandler = new RequestHandler();
+
+                HashMap<String, String> params = new HashMap<>();
+                params.put("name", "Archit");
+
+                return requestHandler.sendPostRequest(URL, params);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Log.d("dashboard", "Error while fetching cases- " + s);
+
+                JSONObject obj = null;
+                try {
+                    obj = new JSONObject(s);
+                    if (obj.getInt("cases")>0) {
+                        cases.setText(String.valueOf(obj.getString("cases")));
+                        recov.setText(String.valueOf(obj.getString("recov")));
+                        death.setText(String.valueOf(obj.getString("death")));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(dashboard.this, "Exception: " + e, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+        Login login = new Login();
+        login.execute();
+
+
+        /*LineChart casesChart = findViewById(R.id.casesChart);
         List<Entry> cases = new ArrayList<>();
 
         cases.add(new Entry(1, 4));
@@ -117,6 +187,6 @@ public class dashboard extends AppCompatActivity {
         deathsChart.getXAxis().setEnabled(false);;
         deathsChart.getAxisRight().setEnabled(false);
         deathsChart.getAxisRight().setGridColor(R.color.colorPrimary);
-        deathsChart.invalidate();
+        deathsChart.invalidate();*/
     }
 }
