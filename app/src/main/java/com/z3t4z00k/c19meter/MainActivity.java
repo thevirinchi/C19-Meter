8156f84912aca1407c1dfdd31dd1fc8eb02f36c8
Z3T4Z00K <agarwal.archit17@gmail.com>
2020-05-03 11:02:50 +0530

    Linked on boarding activities

--- app/src/main/java/com/z3t4z00k/c19meter/MainActivity.java
+++ app/src/main/java/com/z3t4z00k/c19meter/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.Objects;
 
@@ -21,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(MainActivity.this, dashboard.class));
+                startActivity(new Intent(MainActivity.this, onBoardingOne.class));
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                 finish();
             }
         }, 3000);
