bdc0622d36689788cd8789bec1b4317abbef2308
Z3T4Z00K <agarwal.archit17@gmail.com>
2020-05-07 02:08:39 +0530

    Now users can select states from map to check cases

--- app/src/main/java/com/z3t4z00k/c19meter/MapsActivity.java
+++ app/src/main/java/com/z3t4z00k/c19meter/MapsActivity.java
@@ -1,11 +1,15 @@
 package com.z3t4z00k.c19meter;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentActivity;
 
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -25,6 +29,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private int COUNTRY = 0, STATE = 1;
     private Boolean jk = false;
+    private ConstraintLayout stats;
+    private TextView stat;
+    private ImageView close;
 
     private String getAddress(LatLng latLng, int code){
         Geocoder geocoder;
@@ -61,15 +68,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         Objects.requireNonNull(mapFragment).getMapAsync(this);
+
+        stats = findViewById(R.id.stats);
+        stat = findViewById(R.id.state);
+        close = findViewById(R.id.close);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stats.setVisibility(View.GONE);
+                mMap.clear();
+            }
+        });
+
         // Add a marker in Sydney and move the camera
         LatLng delhi = new LatLng(28.38, 77.12);
-        mMap.addMarker(new MarkerOptions().position(delhi).title("Marker in Delhi"));
+        //mMap.addMarker(new MarkerOptions().position(delhi).title("Marker in Delhi"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(delhi));
 
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
@@ -79,13 +98,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Log.d("MapsActivity", "Country= " + country);
                 String state = "";
                 if(country == null || country.equals("India")) {
-                    if(country == null)
-                        jk = true;
+                    jk = country == null;
                     if(jk)
                         state = "Jammu & Kashmir";
                     else
                         state = getAddress(latLng, STATE);
-                    Toast.makeText(getApplicationContext(), state, Toast.LENGTH_SHORT).show();
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("Cases in " + state));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    stats.setVisibility(View.VISIBLE);
+                    stat.setText(state);
                 }
 
             }
