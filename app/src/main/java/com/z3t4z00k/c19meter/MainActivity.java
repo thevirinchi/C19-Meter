3df5583f57e516c1bdfebb0b4bca0852b8089bbe
Z3T4Z00K <agarwal.archit17@gmail.com>
2020-05-08 01:57:59 +0530

    Removed dead code

--- app/src/main/java/com/z3t4z00k/c19meter/MainActivity.java
+++ app/src/main/java/com/z3t4z00k/c19meter/MainActivity.java
@@ -8,31 +8,15 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
-
-import com.parse.LogInCallback;
-import com.parse.Parse;
-import com.parse.ParseAnonymousUtils;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseSession;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.sql.Time;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Objects;
-import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,18 +31,6 @@ public class MainActivity extends AppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).hide();
-
-        /*if(ParseUser.getCurrentUser().getUsername() == null)
-            ParseUser.logInInBackground("", "", new LogInCallback(){
-                @Override
-                public void done(ParseUser user, ParseException e) {
-                    if(user != null)
-                        Log.d("MainActivity", "ParseUser- " + user + " Logged in!");
-                    else
-                        Log.d("MainActivity", "ParseUser- Error- " + e.getMessage());
-                }
-            });*/
-
         @SuppressLint("StaticFieldLeak")
         class Updates extends AsyncTask<Void, Void, String> {
 
@@ -162,14 +134,5 @@ public class MainActivity extends AppCompatActivity {
 
         Login login = new Login();
         login.execute();
-
-        /*new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(MainActivity.this, onBoardingOne.class));
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-                finish();
-            }
-        }, 3000);*/
     }
 }
