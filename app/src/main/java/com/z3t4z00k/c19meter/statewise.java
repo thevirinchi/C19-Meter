d1fb1d3363c1350b903f8090ce699949788ab12e
Z3T4Z00K <agarwal.archit17@gmail.com>
2020-05-09 21:00:25 +0530

    Added state-wise list with their figures

--- app/src/main/java/com/z3t4z00k/c19meter/statewise.java
+++ app/src/main/java/com/z3t4z00k/c19meter/statewise.java
@@ -1,6 +1,8 @@
 package com.z3t4z00k.c19meter;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.data.LineData;
@@ -12,14 +14,23 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class statewise extends AppCompatActivity {
+
+    private String URL = "https://zetazook.club/c19/allstates.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +60,50 @@ public class statewise extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        @SuppressLint("StaticFieldLeak")
+        class Login extends AsyncTask<Void, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                RequestHandler requestHandler = new RequestHandler();
+
+                HashMap<String, String> params = new HashMap<>();
+                params.put("name", "Archit");
+
+                return requestHandler.sendPostRequest(URL, params);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                JSONObject obj;
+                JSONArray jsonArray;
+                try {
+                    jsonArray = new JSONArray(s);
+                    obj = jsonArray.getJSONObject(0);
+                    if (!obj.getString("s").equals("")) {
+                        for (int i = 0; i<33; i++) {
+                            obj = jsonArray.getJSONObject(i);
+                            Log.d("statewise", "Response- " + obj);
+                            stateModals.add(new StateModal(obj.getString("s"),obj.getString("t"),obj.getString("c"),obj.getString("d")));
+                            stateListAdapter.notifyDataSetChanged();
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(statewise.this, "Exception: " + e, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        Login login = new Login();
+        login.execute();
+
     }
 
     @Override
