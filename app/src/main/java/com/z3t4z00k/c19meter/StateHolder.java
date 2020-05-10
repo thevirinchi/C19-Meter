dd6b626038269fd994392db2ef5fbce93527c3e9
Z3T4Z00K <agarwal.archit17@gmail.com>
2020-05-10 20:36:51 +0530

    Implemented recycler view on item click listener

--- app/src/main/java/com/z3t4z00k/c19meter/StateHolder.java
+++ app/src/main/java/com/z3t4z00k/c19meter/StateHolder.java
@@ -6,16 +6,19 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class StateHolder extends RecyclerView.ViewHolder {
+public class StateHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     private TextView state, total, cured, death;
+    stateListAdapter.OnStateListener onStateListener;
 
-    public StateHolder(@NonNull View itemView) {
+    public StateHolder(@NonNull View itemView, stateListAdapter.OnStateListener onStateListener) {
         super(itemView);
         state = itemView.findViewById(R.id.name);
         total = itemView.findViewById(R.id.active);
         cured = itemView.findViewById(R.id.cured);
         death = itemView.findViewById(R.id.deaths);
+        this.onStateListener = onStateListener;
+        itemView.setOnClickListener(this);
     }
 
     void bind(StateModal stateModal){
@@ -25,4 +28,8 @@ public class StateHolder extends RecyclerView.ViewHolder {
         death.setText(stateModal.getD());
     }
 
+    @Override
+    public void onClick(View v) {
+        onStateListener.onStateClick(getAdapterPosition());
+    }
 }
--- app/src/main/java/com/z3t4z00k/c19meter/stateListAdapter.java
+++ app/src/main/java/com/z3t4z00k/c19meter/stateListAdapter.java
@@ -12,19 +12,21 @@ import java.util.List;
 
 public class stateListAdapter extends RecyclerView.Adapter {
 
+    private OnStateListener mOnStateListener;
     private Context context;
     private List<StateModal> stateModalList;
 
-    stateListAdapter(Context context, List<StateModal> stateModals){
+    stateListAdapter(Context context, List<StateModal> stateModals, OnStateListener onStateListener){
         this.context = context;
         this.stateModalList = stateModals;
+        this.mOnStateListener = onStateListener;
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.state_item_layout, parent, false);;
-        return new StateHolder(view);
+        return new StateHolder(view, mOnStateListener);
     }
 
     @Override
@@ -36,5 +38,9 @@ public class stateListAdapter extends RecyclerView.Adapter {
     @Override
     public int getItemCount() {
         return stateModalList.size();
+    }
+
+    public interface OnStateListener{
+        void onStateClick(int position);
     }
 }
